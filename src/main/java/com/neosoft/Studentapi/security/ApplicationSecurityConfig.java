53647b0fc88183ac815002e1eefb390fa6bfc78f
khushbookedia <khushboo.3990@gmail.com>
2021-12-03 17:16:31 +0530

    Added CSRF Token

--- src/main/java/com/neosoft/Studentapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/neosoft/Studentapi/security/ApplicationSecurityConfig.java
@@ -33,7 +33,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .csrf().disable()
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .authorizeRequests()
